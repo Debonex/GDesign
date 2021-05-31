1daeffdd1de5980c043aaa078ebb8f848e878a9b
Debonet <719893090@qq.com>
2021-05-31 18:13:48 +0800

    +contentbase

---
+++ pysrc/algorithms/contentBase/contentBase.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+import pymysql.cursors
+import os
+import json
+import math
+
+
+class contentBase:
+
+    def __init__(self, ranknum, recnum) -> None:
+        self.load_data()
+        self.ranknum = ranknum
+        self.recnum = recnum
+
+    def save_data(self):
+        if os.path.exists('data/train.json') and os.path.exists('data/test.json'):
+            return
+
+        train = {}
+        test = {}
+        connection = pymysql.connect(
+            host='localhost', user='root', password='root', database='gdesign', charset='utf8mb4')
+
+        with connection.cursor() as cursor:
+            sql = """select id_order,uid,id_commodity,num_commodity from orders"""
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            for row in results:
+                # test set
+                if(row[0][5:7] != "12"):
+                    if row[1] in test:
+                        if row[2] in test[row[1]]:
+                            test[row[1]][row[2]] = test[row[1]][row[2]]+row[3]
+                        else:
+                            test[row[1]][row[2]] = row[3]
+                    else:
+                        test[row[1]] = {row[2]: row[3]}
+                # train set
+                else:
+                    if row[1] in train:
+                        if row[2] in train[row[1]]:
+                            train[row[1]][row[2]] = train[row[1]][row[2]]+row[3]
+                        else:
+                            train[row[1]][row[2]] = row[3]
+                    else:
+                        train[row[1]] = {row[2]: row[3]}
+
+        json.dump(train, open("data/train.json", "w"))
+        json.dump(test, open("data/test.json", "w"))
+        connection.close()
+        print("saving train set and test set finished......")
+
+    def pearson(self, u1, u2):
+        num = 0
+        xy, x, y, x2, y2 = 0, 0, 0, 0, 0
+        for key in u1:
+            if key in u2:
+                num += 1
+                x += u1[key]
+                y += u2[key]
+                xy += u1[key]*u2[key]
+                x2 += math.pow(u1[key], 2)
+                y2 += math.pow(u2[key], 2)
+        if num == 0:
+            return 0
+        # 近似的皮尔森表达式
+        denominator = math.sqrt(x2-math.pow(x, 2)/num) * \
+            math.sqrt(y2-math.pow(y, 2)/num)
+        if denominator == 0:
+            return 0
+        return (xy-x*y/num)/denominator
+
+    def load_data(self):
+        if not (os.path.exists('data/train.json') and os.path.exists('data/test.json')):
+            self.save_data()
+        self.train = json.load(open("data/train.json"))
+        self.test = json.load(open("data/test.json"))
+        print("loading data completed......")
+
+    def recommend(self, uid):
+        distance_dict = {}
+        for k in self.train.keys():
+            if k != uid:
+                distance = self.pearson(self.train[uid], self.train[k])
+                distance_dict[k] = distance
+        distance_sorted = sorted(
+            distance_dict.items(), key=lambda x: x[1], reverse=True)
+        commodity_dict = {}
+        for (id, sim) in distance_sorted[:self.ranknum]:
+            for cid in self.train[id]:
+                commodity_dict.setdefault(cid, 0)
+                commodity_dict[cid] += sim*self.train[id][cid]
+        commodity_sorted = sorted(
+            commodity_dict.items(), key=lambda x: x[1], reverse=True)
+        return commodity_sorted[:self.recnum]
+
+
+if __name__ == '__main__':
+    control = contentBase(ranknum=10, recnum=4)
+    rec = control.recommend("120405")
+    print(rec)
