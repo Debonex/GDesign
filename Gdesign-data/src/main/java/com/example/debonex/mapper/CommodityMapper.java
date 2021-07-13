42165fe46193b13c11635dd02593cee46cf2471f
chenliusheng <chen.liusheng@polixir.ai>
2021-07-13 17:34:15 +0800

    update for new environment

--- Gdesign-data/src/main/java/com/example/debonex/mapper/CommodityMapper.java
+++ Gdesign-data/src/main/java/com/example/debonex/mapper/CommodityMapper.java
@@ -1,7 +1,9 @@
 package com.example.debonex.mapper;
 
 import com.example.debonex.pojo.Commodity;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface CommodityMapper {
 
     public Commodity selectById(Integer id);
--- Gdesign-rec/apps/handlers/rec_handler.py
+++ Gdesign-rec/apps/handlers/rec_handler.py
@@ -2,12 +2,13 @@ import tornado.web
 
 import sys
 import os
-sys.path.append('..')
-from algorithms.contentBase.ContentBase import ContentBase
+from apps.algorithms.contentBase.ContentBase import ContentBase
 
 cb = ContentBase()
+
 
 class RecHandler(tornado.web.RequestHandler):
     def get(self):
         res = cb.recommend(120405)
-        self.finish(res)
+        print({"data": res})
+        self.finish({"data": res})
